37631f2cb7e9e0f967c7477a93281bb0b622082d
Rageshwaran <ragesh.1995@gmail.com>
2021-10-06 08:36:31 +0530

    first commit

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+
+import RazorpayCheckout from 'react-native-razorpay';
 
 export default function App() {
+  return (
+    <TouchableHighlight style={styles.container} onPress={() => {
+        var options = {
+        description: 'Credits towards consultation',
+        image: 'https://i.imgur.com/3g7nmJC.png',
+        currency: 'INR',
+        key: '<YOUR_KEY_ID>',
+        amount: '5000',
+        name: 'Acme Corp',
+        order_id: 'order_DslnoIgkIDL8Zt',//Replace this with an order_id created using Orders API.
+        prefill: {
+          email: 'gaurav.kumar@example.com',
+          contact: '9191919191',
+          name: 'Gaurav Kumar'
+        },
+        theme: {color: '#53a20e'}
+      }
+      RazorpayCheckout.open(options).then((data) => {
+        // handle success
+        alert(`Success: ${data.razorpay_payment_id}`);
+      }).catch((error) => {
+        // handle failure
+        alert(`Error: ${error.code} | ${error.description}`);
+      });
+    }}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TouchableHighlight>
+  );
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
